eb6fc55eac17cbcc7e205e60d469cb28aae9c868
Jagdish1102 <jagdishbhoi1102@gmail.com>
2025-06-11 14:28:13 +0530

    Update Category.java

--- Smart-Interview-Prep-Tracker/src/main/java/com/interview_tracker/Entity/Category.java
+++ Smart-Interview-Prep-Tracker/src/main/java/com/interview_tracker/Entity/Category.java
@@ -1,5 +1,47 @@
 package com.interview_tracker.Entity;
 
 public class Category {
+	int id;
+	String name; // e.g., "Java", "DBMS", "HR"
+	String discription; // (Optional) more details
+
+	// --------------------------------------------------------------------------
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDiscription() {
+		return discription;
+	}
+//------------------------------------------------------------------------------
+
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(int id, String name, String discription) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.discription = discription;
+	}
 
 }
+
