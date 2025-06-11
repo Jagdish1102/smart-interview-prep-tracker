bad8f4c60d2d01d6cda20489b827faa673eaed77
Jagdish1102 <jagdishbhoi1102@gmail.com>
2025-06-11 14:21:42 +0530

    Add files via upload

---
+++ Smart-Interview-Prep-Tracker/src/main/java/com/interview_tracker/Entity/Progress.java
@@ -0,0 +1,5 @@
+package com.interview_tracker.Entity;
+
+public class Progress {
+
+}
---
+++ Smart-Interview-Prep-Tracker/src/main/java/com/interview_tracker/SmartInterviewPrepTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.interview_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInterviewPrepTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInterviewPrepTrackerApplication.class, args);
+	}
+
+}
