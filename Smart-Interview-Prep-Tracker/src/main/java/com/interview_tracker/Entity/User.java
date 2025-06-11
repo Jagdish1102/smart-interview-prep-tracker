9a6f614d3ab16cb69f6ed068b624803a815c151c
Jagdish1102 <jagdishbhoi1102@gmail.com>
2025-06-11 14:27:13 +0530

    Update User.java

--- Smart-Interview-Prep-Tracker/src/main/java/com/interview_tracker/Entity/User.java
+++ Smart-Interview-Prep-Tracker/src/main/java/com/interview_tracker/Entity/User.java
@@ -1,5 +1,87 @@
 package com.interview_tracker.Entity;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "User")
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id; // Primary key
+	@Column(name = "name")
+	String name;
+
+	@Column(name = "email")
+	String email; // Unique email ID
+	
+	@Column(name = "password")
+	String password; // Encrypted password/4
+	
+	@Column(name = "role")
+	String role; // "USER" or "ADMIN"
+
+	// --------------------------------------------------------------------------------
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	// --------------------------------------------------------------------------------
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int id, String name, String email, String password, String role) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
 
 }
